c30ca39bd1c26c15fd37dc4660a5785039911ee9
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2023-11-15 11:20:06 +0400

    added favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="shortcut icon" href="/favicon.png" />
+        <meta name="description" content="Chat App" />
+      </Head>
       <body>
         <Main />
         <NextScript />
