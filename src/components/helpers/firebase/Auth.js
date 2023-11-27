1f5c19e7aecb5037485067c025cb4584fd0afa35
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2023-11-27 11:07:07 +0400

    removed extra import

--- src/components/helpers/firebase/Auth.js
+++ src/components/helpers/firebase/Auth.js
@@ -5,7 +5,7 @@ import {
   signOut,
 } from 'firebase/auth';
 import { auth } from './config';
-import { collection, getFirestore, setDoc, doc } from 'firebase/firestore';
+import {  getFirestore, setDoc, doc } from 'firebase/firestore';
 
 const db = getFirestore();
 
