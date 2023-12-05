a59ca2b05b769547da4da9d2e1ff4dd9764b6001
giorgi adu <103452467+gioAdu@users.noreply.github.com>
2023-12-05 17:37:44 +0400

    chaned UI in sidebar

--- src/components/UI/Layout/Sidebar.js
+++ src/components/UI/Layout/Sidebar.js
@@ -3,12 +3,13 @@ import LightModeIcon from '@mui/icons-material/LightMode';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import ChatIcon from '@mui/icons-material/Chat';
 import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 import { useTheme } from 'next-themes';
 
-import Link from 'next/link';
 
 import { logoutFunc } from '../../firebase/Auth';
+import { pink } from '@mui/material/colors';
 
 const SideBar = ({ setCurrentSection }) => {
   const { resolvedTheme, setTheme } = useTheme();
@@ -19,8 +20,7 @@ const SideBar = ({ setCurrentSection }) => {
     setCurrentSection(section);
   };
 
-  const handleLogout = async (e) => {
-    e.preventDefault();
+  const handleLogout = async () => {
     await logoutFunc();
     localStorage.removeItem('chat');
     localStorage.removeItem('section');
@@ -63,22 +63,25 @@ const SideBar = ({ setCurrentSection }) => {
           <ChatIcon color="action" />
           <ListItemText primary="Chats" />
         </ListItemButton>
-      </Box>
 
-      <Box>
         <IconButton
-          sx={{ width: '100%', borderRadius: 0 }}
+          sx={{ borderRadius: 0 }}
           aria-label={`switch to ${resolvedTheme === 'light' ? 'dark' : 'light'} mode`}
           onClick={() => setTheme(resolvedTheme === 'light' ? 'dark' : 'light')}
         >
           {resolvedTheme === 'light' ? <DarkModeIcon /> : <LightModeIcon />}
         </IconButton>
+      </Box>
 
-        <Link href="/" onClick={handleLogout}>
-          <ListItemButton>
-            <ListItemText primary="Log out" />
-          </ListItemButton>
-        </Link>
+      <Box>
+        <ListItemButton
+          color="action"
+          onClick={handleLogout}
+          sx={{ display: 'flex', flexDirection: 'column' }}
+        >
+          <LogoutIcon sx={{ color: pink[500] }} />
+          Log out
+        </ListItemButton>
       </Box>
     </List>
   );
